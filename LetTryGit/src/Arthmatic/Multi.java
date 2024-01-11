31eb80dd953c581388ebe1b2338ddb009ce3b8f6
Meeghaa <meghagangavane69@gmail.com>
2024-01-11 16:01:54 +0530

    Multipication Added

---
+++ LetTryGit/src/Arthmatic/Multi.java
@@ -0,0 +1,12 @@
+package Arthmatic;
+
+public class Multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=2;
+		System.out.println("Mutiplication="+(a*b));
+	}
+
+}
